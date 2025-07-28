6d251e6d743bc3acdecf79e08a7cf8816187f76a
Shumpei Fujita <shfujita@c5r1s1.42tokyo.jp>
2025-07-28 20:46:16 +0900

    250728

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,11 +6,87 @@
 /*   By: shfujita <shfujita@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 16:13:09 by shfujita          #+#    #+#             */
-/*   Updated: 2025/07/26 18:24:52 by shfujita         ###   ########.fr       */
+/*   Updated: 2025/07/28 20:44:02 by shfujita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	*bubble_sort(int *value, int size)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	if (!value)
+		return (NULL);
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (value[i] > value[j])
+			{
+				tmp = value[i];
+				value[i] = value[j];
+				value[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (value);
+}
+
+int	*stack_to_array(t_stack *stack)
+{
+	int		size;
+	int		*array;
+	int		i;
+	t_stack	*current;
+
+	current = stack;
+	i = 0;
+	size = stack_size(stack);
+	array = malloc(sizeof(int) * size);
+	if (!array)
+		return (NULL);
+	while (i < size)
+	{
+		array[i] = current->data;
+		current = current->next;
+		i++;
+	}
+	return (array);
+}
+
+static void	assign_index(t_stack *stack_a)
+{
+	int		size;
+	int		*array;
+	int		rank;
+	int		i;
+	t_stack	*current;
+
+	if (!stack_a)
+		return ;
+	i = 0;
+	size = stack_size(stack_a);
+	array = bubble_sort(stack_to_array(stack_a), size);
+	if (!array)
+		return ;
+	current = stack_a;
+	while (i < size)
+	{
+		rank = 0;
+		while (rank < size && array[rank] != current->data)
+			rank++;
+		current->index = rank;
+		current = current->next;
+	}
+	free(array);
+}
 
 int	main(int argc, char *argv[])
 {
--- push_swap/small_size_sort.c
+++ push_swap/small_size_sort.c
@@ -6,11 +6,39 @@
 /*   By: shfujita <shfujita@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 18:35:12 by shfujita          #+#    #+#             */
-/*   Updated: 2025/07/26 19:53:18 by shfujita         ###   ########.fr       */
+/*   Updated: 2025/07/28 16:35:31 by shfujita         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	find_min_id(t_stack *stack_a)
+{
+	t_stack	*current;
+	int		min_data;
+	int		idx;
+	int		min_idx;
+
+	if (!stack_a)
+		return (-1);
+	idx = 0;
+	min_idx = 0;
+	current = stack_a;
+	min_data = current->data;
+	while (1)
+	{
+		if (current->data < min_data)
+		{
+			min_idx = idx;
+			min_data = current->data;
+		}
+		idx++;
+		current = current->next;
+		if (current == stack_a)
+			break ;
+	}
+	return (min_idx);
+}
 
 void	three_sort(t_stack **stack_a)
 {
@@ -37,4 +65,48 @@ void	three_sort(t_stack **stack_a)
 		r_rotate(stack_a, 'a');
 	else if (first > second && second < third && first > third)
 		rotate(stack_a, 'a');
+}
+
+void	four_sort(t_stack **stack_a, t_stack **stack_b)
+{
+	int	size_a;
+	int	min_id;
+	int	back;
+
+	min_id = find_min_id(*stack_a);
+	size_a = stack_size(*stack_a);
+	if (min_id <= size_a / 2)
+		while (min_id-- > 0)
+			rotate(stack_a, 'a');
+	else
+	{
+		back = size_a - min_id;
+		while (back-- > 0)
+			r_rotate(stack_a, 'a');
+	}
+	push(stack_a, stack_b, 'b');
+	three_sort(stack_a);
+	push(stack_b, stack_a, 'a');
+}
+
+void	five_sort(t_stack **stack_a, t_stack **stack_b)
+{
+	int	size_a;
+	int	min_id;
+	int	back;
+
+	size_a = stack_size(*stack_a);
+	min_id = find_min_id(*stack_a);
+	if (min_id <= size_a / 2)
+		while (min_id-- > 0)
+			rotate(stack_a, 'a');
+	else
+	{
+		back = size_a - min_id;
+		while (back-- > 0)
+			r_rotate(stack_a, 'a');
+	}
+	push(stack_a, stack_b, 'b');
+	four_sort(stack_a, stack_b);
+	push(stack_b, stack_a, 'a');
 }
